fdeb9a523776e4cca45fa2aac80412dffd1fa5f8
Stefan Oehmcke <stefan.oehmcke@uni-oldenburg.de>
2024-06-20 21:39:28 +0200

    ffcv test

---
+++ data/convert2ffcv.py
@@ -0,0 +1,18 @@
+import torch
+from ffcv.writer import DatasetWriter
+from ffcv.fields import TorchTensorField, IntField, BytesField
+
+def convert_mmearth(dataset):
+    # Your dataset (`torch.utils.data.Dataset`) of (image, label) pairs
+    write_path = 'data/mmearth.beton'
+
+    # Pass a type for each data field
+    writer = DatasetWriter(write_path, {
+        # Tune options to optimize dataset size, throughput at train-time
+        'sentinel2': TorchTensorField(dtype=torch.float, shape=(12, 128, 128)),
+        'biome': IntField(),
+        'id': BytesField(),
+    })
+
+    # Write dataset
+    writer.from_indexed_dataset(dataset)
---
+++ tests/convert_test.py
@@ -0,0 +1,24 @@
+import pytest
+from torch import Tensor
+
+from data import constants
+from data.constants import MMEARTH_DIR
+from data.convert2ffcv import convert_mmearth
+from data.geobench_dataset import GeobenchDataset
+from data.mmearth_dataset import MultimodalDataset, create_MMEearth_args
+
+
+@pytest.mark.parametrize("split", ["train"])
+@pytest.mark.parametrize(
+    "modalities",
+    [constants.INP_MODALITIES],
+)
+def test_mmearth_dataset(split, modalities):
+    args = create_MMEearth_args(
+        MMEARTH_DIR, modalities, {"biome": constants.MODALITIES_FULL["biome"]}
+    )
+
+    args.modalities = modalities
+    dataset = MultimodalDataset(args, split=split, transform=None)
+
+    convert_mmearth(dataset)
