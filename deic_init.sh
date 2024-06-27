b2516a57e85dac50894ea95c86ac15b6c1e38134
Ankit <ankit.ky@gmail.com>
2024-06-27 15:17:35 +0200

    Add opencv requirenents to deic init

--- deic_init.sh
+++ deic_init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo apt-get update && sudo apt-get install -y libgl1-mesa-dev
 eval "$(conda shell.bash hook)"
 # Check if dir /work/cenv/ssl4eo exists; create conda env if not
 if [ -d "/work/project/cenv/ssl4eo" ]; then
@@ -9,6 +10,7 @@ else
     mamba env create --prefix /work/project/cenv/ssl4eo -f /work/data/env.yml
     export CONDA_ENVS_DIRS="/work/project/cenv/"
 fi
+conda activate ssl4eo
 # Export new python kernel
 python -m ipykernel install --user --name ipy39 --display-name "SSL4EO"
 
